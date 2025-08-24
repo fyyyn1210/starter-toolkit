c10f4538ea07228b2f499f2a2ce79ab2b8067717
fyyyn1210 <riosraskaa@gmail.com>
2025-08-24 15:06:55 +0700

    ( Feat ) : <templates> adding Sequlize Express Project

---
+++ templates/express-sequelize/src/app.js
@@ -0,0 +1,16 @@
+import express from "express";
+import 'dotenv/config';
+import { initDB } from "./configs/sequelize.config.js";
+import mainRoute from "./routes/main.route.js";
+
+const app = express();
+app.use(express.json());
+// Routers
+app.use('/api', mainRoute);
+
+const PORT = process.env.APP_PORT || 3000;
+
+initDB();
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
