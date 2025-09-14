4f1865d2ab5e410b23926068def0d413ca4fb14a
fyyyn1210 <riosraskaa@gmail.com>
2025-09-14 16:24:36 +0700

    ( FIx )

--- templates/express-sequelize/src/app.js
+++ templates/express-sequelize/src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import 'dotenv/config';
 import { initDB } from "./configs/sequelize.config.js";
 import mainRoute from "./routes/main.route.js";
+import setupAssociations from "./database/models/model.associations.js";
 
 const app = express();
 app.use(express.json());
@@ -10,7 +11,8 @@ app.use('/api', mainRoute);
 
 const PORT = process.env.APP_PORT || 3000;
 
-initDB();
+setupAssociations();
+(async ()=> await initDB())();
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
